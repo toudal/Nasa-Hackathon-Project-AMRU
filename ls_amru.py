bb6d8a75ba5a7576a123f23829e5d48ce59fb98f
toudal <toudal@users.noreply.github.com>
2016-04-24 18:44:51 +0200

    Update ls_amru.py

--- ls_amru.py
+++ ls_amru.py
@@ -3,8 +3,13 @@
 import os
 import sys
 
+def _visited():
+    return True
+
+        
 def main():
-
-
+    you_r = _visited()
+    ls_pcd = env.compile()
+    
 if __name__ == "__main__":
     main()
